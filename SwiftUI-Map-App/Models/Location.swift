63e95f259a6ba31a92ed24af71ce6ee383bef996
sajid <sajidhasan673@gmail.com>
2022-11-28 22:37:34 +0600

    create location model and add data services

---
+++ SwiftUI-Map-App/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  SwiftUI-Map-App
+//
+//  Created by Sajid Shanta on 28/11/22.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
