0593355b764940a0fbdef544f903fb98e2d931f1
sajid <sajidhasan673@gmail.com>
2022-11-28 22:11:17 +0600

    initialize the project  and add assets

---
+++ SwiftUI-Map-App/SwiftUI_Map_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Map_AppApp.swift
+//  SwiftUI-Map-App
+//
+//  Created by Sajid Shanta on 28/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Map_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
