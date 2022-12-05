635717338f24b58a33dc5254c5a996fbd09add84
sajid <sajidhasan673@gmail.com>
2022-12-05 23:58:56 +0600

    connect map with ViewModel

--- SwiftUI-Map-App/ViewModels/LocationsViewModel.swift
+++ SwiftUI-Map-App/ViewModels/LocationsViewModel.swift
@@ -6,11 +6,32 @@
 //
 
 import Foundation
+import MapKit
 
 class LocationsViewModel: ObservableObject {
     @Published var locations: [Location]
     
+    @Published var mapLocation: Location {
+        didSet {
+            updateMapRegion(location: mapLocation)
+        }
+    }
+    
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    
     init(){
-        self.locations = LocationsDataService.locations
+        let locations = LocationsDataService.locations
+        self.locations = locations
+        self.mapLocation = locations.first!
+        
+        self.updateMapRegion(location: locations.first!)
+    }
+    
+    func updateMapRegion(location: Location) {
+        self.mapRegion = MKCoordinateRegion(
+            center: location.coordinates,
+            span: mapSpan)
     }
 }
--- SwiftUI-Map-App/Views/LocationsView.swift
+++ SwiftUI-Map-App/Views/LocationsView.swift
@@ -10,13 +10,14 @@ import MapKit
 
 struct LocationsView: View { 
     @EnvironmentObject private var vm: LocationsViewModel
-    @State private var mapRegion: MKCoordinateRegion = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 41.8986, longitude: 12.4769),
-        span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+    
+//    @State private var mapRegion: MKCoordinateRegion = MKCoordinateRegion(
+//        center: CLLocationCoordinate2D(latitude: 41.8986, longitude: 12.4769),
+//        span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
     
     var body: some View {
         ZStack {
-            Map(coordinateRegion: $mapRegion)
+            Map(coordinateRegion: $vm.mapRegion)
                 .ignoresSafeArea()
         }
 //        List {
