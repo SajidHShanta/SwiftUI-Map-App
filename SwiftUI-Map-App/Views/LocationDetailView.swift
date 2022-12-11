ad134f05d51351a3d4a25a122120910d6d191a9a
sajid <sajidhasan673@gmail.com>
2022-12-12 05:17:27 +0600

    create detailed location view page

---
+++ SwiftUI-Map-App/Views/LocationDetailView.swift
@@ -0,0 +1,96 @@
+//
+//  LocationDetailView.swift
+//  SwiftUI-Map-App
+//
+//  Created by Sajid Shanta on 12/12/22.
+//
+
+import SwiftUI
+import MapKit
+
+struct LocationDetailView: View {
+    let location: Location
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                imageSection
+                    .shadow(color: .black.opacity(0.3), radius: 20, y: 10)
+                
+                VStack(alignment: .leading, spacing: 16) {
+                    titleSection
+                    Divider()
+                    descriptionSection
+                    Divider()
+                    mapLayer
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+            }
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct LocationDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationDetailView(location: LocationsDataService.locations.first!)
+    }
+}
+
+extension LocationDetailView {
+    private var imageSection: some View {
+        TabView {
+            ForEach(location.imageNames, id: \.self) {
+                Image($0)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: UIScreen.main.bounds.width)
+                    .clipped()
+            }
+        }
+        .frame(height: 500)
+        .tabViewStyle(PageTabViewStyle())
+    }
+    
+    private var titleSection: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text(location.name)
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+            
+            Text(location.cityName)
+                .font(.title3)
+                .foregroundColor(.secondary)
+        }
+    }
+    
+    private var descriptionSection: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            Text(location.description)
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+            
+            if let url = URL(string: location.link) {
+                Link("Read more on wikipedia", destination: url)
+                    .font(.headline)
+                    .tint(.blue)
+            }
+        }
+    }
+    
+    private var mapLayer: some View {
+        Map(coordinateRegion: .constant(MKCoordinateRegion(
+            center: location.coordinates,
+            span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))),
+            annotationItems: [location],
+            annotationContent: { location in
+            MapAnnotation(coordinate: location.coordinates) {
+                MapAnnotationView()
+                    .shadow(color: .black.opacity(0.3), radius: 10)
+            }
+        })
+            .aspectRatio(1, contentMode: .fit)
+            .allowsHitTesting(false) //disable gesture on map
+    }
+}
